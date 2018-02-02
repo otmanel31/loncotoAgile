497ccfc4153e2f92266160a131fb7b7adb2f0f88
Stagiaire <Stagiaire@MININT-DEAA2P1.mshome.net>
2018-02-02 10:12:22 +0100

    creer les repository (sans gestion planning) (2 SP) khalid

---
+++ loncotoAgile/src/main/java/com/loncoto/loncontoBoot/repositories/EtageRepository.java
@@ -0,0 +1,9 @@
+package com.loncoto.loncontoBoot.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.loncoto.loncontoBoot.metier.Etage;
+
+public interface EtageRepository extends PagingAndSortingRepository<Etage, Integer> {
+
+}
